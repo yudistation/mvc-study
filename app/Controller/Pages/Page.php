02543ba9e0fd7dc74351a81d91aefa6281650d49
yudistation <gabryudi@gmail.com>
2022-01-13 14:47:38 -0400

    Added footer and header

--- app/Controller/Pages/Page.php
+++ app/Controller/Pages/Page.php
@@ -5,14 +5,24 @@ use \App\Utils\View;
 
 class Page{
 
+    /**
+     * Método responsável por renderizar o topo da página
+     * @return string
+     */
+    private static function getHeader(){
+        return View::render('pages/header');
+    }
+
+
     /* Método responsável por retornar o conteúdo (view) da nossa home
      @return string
      */
     
     public static function getPage($title,$content) {
         return View::render('pages/page', [
-            'title' => 'paginas',
-            'content' => 'Canal do Youtube: https://youtube.com/yudistation'
+            'title' => $title,
+            'header' => self::getHeader(),
+            'content' => $content
         ]);
     }
 
